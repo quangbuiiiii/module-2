3c999b1794c55a0cc2e89a92112dac477e2a2c8b
quangbuiiiii <quangbd.GB0117030958@dewey.edu.vn>
2024-09-19 20:48:46 +0700

    Add files via upload

---
+++ Students.java
@@ -0,0 +1,17 @@
+package ss5.bai_tap.students;
+
+public class Students {
+    private String name="john";
+    private String classes= "C02";
+    public Students(){}
+    public  void setName(String name){
+        this.name=name;
+    }
+    public void setClasses(String classes){
+        this.classes=classes;
+    }
+    public String displayInformation(){
+        return "Name :"+this.name+"\nClasses :"+this.classes;
+    }
+}
+
